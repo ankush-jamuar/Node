0c0c5811da059a2fa889bb6cdddab4b187a08773
ankush <jamuar66@gmail.com>
2025-12-11 11:15:24 +0530

    good

--- postgre/postgrescrud.js
+++ postgre/postgrescrud.js
@@ -1,24 +1,38 @@
-import pool from './db.js';
+import pool from "./db.js";
 
-const createmovie = async(id, name, imdb, language)=>{
-    await pool.query(
-        "insert into movies(id, name, imdb, language) values($1, $2, $3, $4)", [id, name, imdb, language]
-    );
-    console.log("Data inserted")
+const createmovie = async (id, name, imdb, language) => {
+  await pool.query(
+    "insert into movies(id, name, imdb, language) values($1, $2, $3, $4)",
+    [id, name, imdb, language]
+  );
+  console.log("Data inserted");
 };
 
-const updatemovie = async(id, language)=>{
-    const result = await pool.query(
-        "update movies set language=$2 where id=$1 returning *", [id, language]
-    )
-    console.log(result.rows)
+const updatemovie = async (id, language) => {
+  const result = await pool.query(
+    "update movies set language=$2 where id=$1 returning *",
+    [id, language]
+  );
+  console.log(result.rows);
 };
 
-async function main(){
-    // await createmovie(5, 'Welcome', 8.2, 'Hindi');    
-    // await createmovie(1, 'Dilwale', 6.1, 'Hindi');
-    await updatemovie(4, 'Hindi');
-    pool.end();
+const getmovies = async () => {
+  const result = await pool.query("select * from movies");
+  console.log(result.rows);
 };
+
+const delmovies = async(id)=>{
+    const result = await pool.query("delete from movies where id=$1 returning *", [id]);
+    console.log(result.rows);
+}
+
+async function main() {
+  // await createmovie(5, 'Welcome', 8.2, 'Hindi');
+  // await createmovie(1, 'Dilwale', 6.1, 'Hindi');
+//   await updatemovie(4, "Hindi");
+    await getmovies();
+    // await delmovies(4);
+  pool.end();
+}
 
 main();
